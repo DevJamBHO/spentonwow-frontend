9dd582d9c820968bb5d12e8acef922896832a0be
OlivierLB <lebloas.olivier@gmail.com>
2024-09-27 12:03:32 +0200

    Corrige l'espacement et ajuste les styles
    Corrige l'espacement pour améliorer la lisibilité du code. Modifie les styles pour rendre la hauteur des conteneurs latéraux indépendante et automatique, assurant une meilleure flexibilité dans l'affichage.

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -8,7 +8,7 @@ import Header from '@/components/Dashboard/Header';
 import AmountSection from '@/components/Dashboard/AmountSection';
 import CardList from '@/components/Dashboard/CardList';
 import styles from '@/styles/Dashboard.module.scss';
-import {translate} from "@/utils/translate";
+import { translate } from "@/utils/translate";
 import Details from "@/components/Dashboard/Details";
 
 const Dashboard: React.FC = () => {
@@ -39,7 +39,7 @@ const Dashboard: React.FC = () => {
             <div className={styles.amountDetails}>
                 <div className={styles.mainContainer}>
                     <Container className={styles.mainContainer}>
-                        <Header/>
+                        <Header />
                         <AmountSection />
                         <CardList />
                     </Container>
